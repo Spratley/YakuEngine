544f61573b0de0fa6ec68104a6aae3e5e6fca8f0
Jacob Pratley <pratleyjacob@gmail.com>
2025-06-01 11:52:45 -0400

    Integrated CMake to build project files

---
+++ CMake/CommonSettings.cmake
@@ -0,0 +1,20 @@
+# C++ Standard
+set(CMAKE_CXX_STANDARD 20)
+set(CMAKE_CXX_STANDARD_REQUIRED ON)
+
+# Use Folders
+set_property(GLOBAL PROPERTY USE_FOLDERS ON)
+
+# Set output folders
+set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/bin)
+set(CMAKE_LIBRARY_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/lib)
+set(CMAKE_ARCHIVE_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/lib)
+
+# Debug Output
+set(CMAKE_DEBUG_POSTFIX "_Debug")
+
+# MSVC Flags
+if (MSVC)
+	add_compile_options(/W4 /WX /MP /permissive-)
+	add_definitions(-DNOMINMAX -D_CRT_SECURE_NO_WARNINGS)
+endif()
