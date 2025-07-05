f204b3bebe15a1416afc37742f61f901d144da99
Jacob Pratley <pratleyjacob@gmail.com>
2025-07-05 12:47:53 -0400

    I'm doing everything I can here *sob*

--- Engine/YK/Libraries/HIDra/HIDraTypes.cpp
+++
@@ -1,6 +0,0 @@
-#include "HIDraTypes.h"
-
-namespace HIDra
-{
-    Vec2f const Vec2f::s_zero;
-}
--- Engine/YK/Libraries/HIDra/Platforms/Windows/Keyboard/HIDraKeyCodeTranslator_Windows.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "../../../HIDraEnums.h"
-
-namespace HIDra
-{
-    KeyCode GetKeycodeFromWindowsVK(HIDra_UInt16 windowsKeyCode, HIDra_UInt16 flags);
-}
